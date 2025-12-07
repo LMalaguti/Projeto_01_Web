f7ef116f1f1a04d2c76068604795f2a618dbeabf
LMalaguti <lucas.p.malaguti@sempreceub.com>
2025-12-06 21:27:16 -0300

    chore: cleanup for release - remove db.sqlite3 from git and secure settings

--- Projeto_01_Web/settings.py
+++ Projeto_01_Web/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qneac4e@d7%k887%if(yd6pzu)dvd%)7=o+f_21nyf0j9fkj4v'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-qneac4e@d7%k887%if(yd6pzu)dvd%)7=o+f_21nyf0j9fkj4v')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
